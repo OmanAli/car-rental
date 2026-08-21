0860662da853c967e2e170f089722cca1f111d3b
Oman Ali <omanali909@gmail.com>
2026-08-21 10:32:53 +0500

    fix: Adjust car image styling for better display and add versioning to CSS links

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -9,8 +9,8 @@
     <link rel="shortcut icon" href="{{ asset('assets/Logo_t.png') }}" />
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800;900&display=swap">
-    <link rel="stylesheet" href="{{ asset('assets/css/plugins.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/plugins.css') }}?v={{ @filemtime(public_path('assets/css/plugins.css')) }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}?v={{ @filemtime(public_path('assets/css/style.css')) }}" />
 </head>
 
 <body>
